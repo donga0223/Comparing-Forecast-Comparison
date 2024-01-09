3747bc306ff92ba9ca795ddb5d212aadd3f90c48
Dongah Kim <donga0223@gmail.com>
2024-01-09 11:09:16 -0500

    model selection

---
+++ Simulation1/dfa_sim_fit_all_withcondition_pq_replicates.py
@@ -0,0 +1,79 @@
+import os
+import argparse
+import pathlib
+import time
+
+def submit_jobs(test_run):
+    shdir = 'simulation/ardfa_sh'
+    pathlib.Path(shdir).mkdir(parents=True, exist_ok=True)
+    logdir = 'simulation/ardfa_logs_pq'
+    pathlib.Path(logdir).mkdir(parents=True, exist_ok=True)
+    savedir = '/work/pi_nick_umass_edu/ARVarDFA/simulation/ardfa_ARVar_fits_pq'
+    
+    if test_run:
+        sample_sizes = ['small']
+        thetas = ['0.0']
+        ps = [1]
+        qs = [1]
+        num_factors = [3]
+        conditions = ['all_constant']
+        num_replicates = 1
+    else:
+        sample_sizes = ['small']
+        thetas = ['0.0', '1.0', '10.0']
+        ps = [1,2,3]
+        qs = [1,2,3]
+        num_factors = [2,3,4,5,6]
+        #conditions = ['all_vary', 'sigma_t_vary', 'sigma_l_vary', 'sigma_h_vary', 'all_constant']
+        conditions = ['all_vary', 'all_constant']
+        num_replicates = 100
+        
+    for sample_size in sample_sizes:
+        for theta in thetas:
+            for p in ps:
+                for q in qs:
+                    for num_factor in num_factors:
+                        for condition in conditions:
+                            for replicate in range(num_replicates):
+                                name = f'{sample_size}_{theta}_{p}_{q}_{num_factor}_{condition}_{replicate}'
+                                samples_file = pathlib.Path(savedir) / f'samples_{name}.pkl'
+                                if samples_file.exists() and samples_file.stat().st_size > 0:
+                                    print(f'Skipping {name}, samples file already exists')
+                                    continue
+                    
+                                cmd = f'module load miniconda/22.11.1-1\n' \
+                                    f'conda activate forecastskill\n' \
+                                    f'python dfa_sim_fit_one_withcondition_pq_replicate.py --sample_size {sample_size} --theta {theta} --p {p} --q {q} --num_factor {num_factor} --condition {condition} --replicate {replicate}'
+                                print(f"Launching {name}")
+                                
+                                sh_contents = f'#!/bin/bash\n' \
+                                            f'#SBATCH --job-name="{name}"\n' \
+                                            f'#SBATCH --ntasks=1 \n' \
+                                            f'#SBATCH -c 1 # Number of Cores per Task\n' \
+                                            f'#SBATCH --nodes=1 # Requested number of nodes\n' \
+                                            f'#SBATCH --mem=4000 # Requested Memory\n' \
+                                            f'#SBATCH --output="{logdir}/{name}.out" \n' \
+                                            f'#SBATCH --error="{logdir}/{name}.err" \n' \
+                                            f'#SBATCH --partition cpu-long # Partition\n' \
+                                            f'#SBATCH --time 250:00:00 # Job time limit\n' + cmd
+                                
+                                shfile = pathlib.Path(shdir) / f'{name}.sh'
+                                with open(shfile, 'w') as f:
+                                    f.write(sh_contents)
+                                
+                                os.system(f'sbatch {shfile}')
+                                time.sleep(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Run estimation for simulation study, multiple combinations of sample size, theta, condition, and replicate index')
+    
+    parser.add_argument('--test_run',
+                        action=argparse.BooleanOptionalAction,
+			help='If specified, run only a small selection of setting for sample size, theta, condition, and replicate index for testing purposes.')
+    parser.set_defaults(test_run=False)
+    
+    args = parser.parse_args()
+    
+    submit_jobs(**vars(args))
+
+    
---
+++ Simulation1/dfa_sim_fit_one_withcondition_pq_replicate.py
@@ -0,0 +1,185 @@
+import sys
+import argparse
+import pathlib
+import pickle
+import time
+
+import jax
+import numpy as np
+from archdfa import ARCHDFA
+from archdfa import log_likelihoods
+from archdfa import log_likelihoods_bar
+
+# ensure that the path where we will save model fits exists
+#save_path = pathlib.Path('/work/pi_nick_umass_edu/simulation/ardfa_fits')
+save_path = pathlib.Path('simulation/ardfa_ARVar_fits_pq')
+if not save_path.exists():
+    save_path.mkdir(parents=True)
+
+
+def get_rng_key(sample_size, theta, condition, replicate):
+    '''
+    Get the RNG key to use for one simulation replicate
+    '''
+    # seeds from random.org
+    # organized by sample size and value of theta
+    seeds = {
+        'small': {
+            '0.0': {
+                'all_vary': 32626,
+                'sigma_t_vary': 194420,
+                'sigma_l_vary': 217934,
+                'sigma_h_vary': 279063,
+                'all_constant': 754766
+            },
+            '1.0': {
+                'all_vary': 163488,
+                'sigma_t_vary': 485464,
+                'sigma_l_vary': 710053,
+                'sigma_h_vary': 12280,
+                'all_constant': 838587
+            },
+            '10.0': {
+                'all_vary': 747507,
+                'sigma_t_vary': 106645,
+                'sigma_l_vary': 972520,
+                'sigma_h_vary': 6818092,
+                'all_constant': 715733
+            },
+        },
+        'large': {
+            '0.0': {
+                'all_vary': 265114,
+                'sigma_t_vary': 209021,
+                'sigma_l_vary': 897409,
+                'sigma_h_vary': 695232,
+                'all_constant': 79225
+            },
+            '1.0': {
+                'all_vary': 914113,
+                'sigma_t_vary': 628716,
+                'sigma_l_vary': 364756,
+                'sigma_h_vary': 757002,
+                'all_constant': 951849
+            },
+            '10.0': {
+                'all_vary': 853825,
+                'sigma_t_vary': 273283,
+                'sigma_l_vary': 217012,
+                'sigma_h_vary': 77890,
+                'all_constant': 683583
+            }
+        }  
+    }
+
+    # split into 1000 RNG keys, one per replicate at this combination of
+    # sample size and theta
+    keys = jax.random.split(jax.random.PRNGKey(seeds[sample_size][theta][condition]),
+                            num=1000)
+    return keys[replicate]
+
+
+def run_simstudy_replicate(sample_size, theta, p, q, num_factors, condition, replicate):
+    '''
+    Fit DFA model and save outputs for one simulation replicate
+    '''
+
+    samples_file = save_path / f'samples_{sample_size}_{theta}_{p}_{q}_{num_factors}_{condition}_{replicate}.pkl'
+    summary_file = save_path / f'summary_{sample_size}_{theta}_{p}_{q}_{num_factors}_{condition}_{replicate}.txt'
+    if samples_file.exists() and samples_file.stat().st_size > 0:
+        print(f'model fit file already exists; skipping {sample_size} {theta} {p} {q} {num_factors} {condition} {replicate}')
+        return
+
+    
+    # load simulated data
+    with open(f'simulation/ardfa_samples/{sample_size}_{theta}_{condition}.pkl', 'rb') as f:
+        obs = pickle.load(f)['y'][replicate, ...]
+    num_samples = 10000
+    num_chains = 3
+    thinning = 10
+    
+    # instantiate and fit model
+    dfa_model = ARCHDFA(num_timesteps=obs.shape[0], num_series=obs.shape[1], 
+                        num_horizons=obs.shape[2],num_factors=num_factors, p=p, q=q, 
+                        sigma_factors_model='AR', 
+                        loadings_constraint='simplex')
+
+
+    mcmc_samples = dfa_model.fit(y=obs, 
+                                rng_key = get_rng_key(sample_size, theta, condition, replicate),
+                                num_warmup=10000, num_samples=num_samples, num_chains=num_chains, thinning = thinning)
+  
+    # calculate DIC
+    start = time.time() 
+    intercept = mcmc_samples['intercept']
+    zHmean = mcmc_samples['zHmean']
+    Omega_tl = mcmc_samples['Omega_tl']
+    llhood = log_likelihoods(intercept, zHmean, Omega_tl, obs, num_samples, num_chains, thinning)
+    #print(llhood.shape)
+
+    D_bar = np.mean(-2*np.sum(llhood, axis = (1,2)))
+    print(D_bar)
+
+    post_means_est = {
+        param: np.mean(mcmc_samples[param], axis=0)[np.newaxis, ...] \
+            for param in ['intercept', 'zHmean', 'Omega_tl']
+    }
+
+    D_theta_bar = log_likelihoods_bar(post_means_est, obs)
+    n_par_eff_est = D_bar - D_theta_bar
+    DIC_est = n_par_eff_est + D_bar
+    print(DIC_est)
+    print('\nDIC calculation time:', time.time() - start)
+
+    # save samples and mcmc summary
+    with open(samples_file, 'wb') as f:
+        pickle.dump(
+            {k: mcmc_samples[k] for k in ['intercept', 'zHmean', 'Omega_tl']},
+            f)
+
+    orig_stdout = sys.stdout
+    with open(summary_file, 'w') as f:
+        sys.stdout = f
+        dfa_model.mcmc.print_summary()
+        print(f"DIC_est: {DIC_est}")
+    sys.stdout = orig_stdout
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Run estimation for simulation study, one sample size, theta, and replicate index')
+    
+    parser.add_argument('--sample_size',
+                        help='sample size',
+                        choices=['small', 'large'],
+                        default='small')
+    parser.add_argument('--theta',
+                        help='parameter value for theta, the mean loss difference',
+                        choices=['0.0', '1.0', '10.0'],
+                        default='0.0')
+    parser.add_argument('--p', type=int,
+                        help='integer index of chosing p',
+                        choices=list(range(1,3)),
+                        default=1)
+    parser.add_argument('--q', type=int,
+                        help='integer index of chosing q',
+                        choices=list(range(1,3)),
+                        default=1)
+    parser.add_argument('--num_factors', type=int,
+                        help='integer index of chosing number of factors',
+                        choices=list(range(1,10)),
+                        default=1)
+    parser.add_argument('--condition',
+                        help='condition',
+                        choices=['all_vary','sigma_t_vary', 'sigma_l_vary', 'sigma_h_vary', 'all_constant'],
+                        default=0)
+    parser.add_argument('--replicate', type=int,
+                        help='integer index of simulation replicate',
+                        choices=list(range(50)),
+                        default=0)
+                        
+    args = parser.parse_args()
+
+    run_simstudy_replicate(**vars(args))
+        
+
