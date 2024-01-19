33a9ce8cda2ec9d33ad7a2b1b1084097ee86d37d
Dongah Kim <donga0223@gmail.com>
2024-01-19 10:56:24 -0500

    change the name of the file

--- Simulation1/sim_fit_all_replicate.py
+++ Simulation1/sim_fit_all_replicate.py
@@ -34,7 +34,7 @@ def submit_jobs(test_run):
         
                     cmd = f'module load miniconda/22.11.1-1\n' \
                         f'conda activate forecastskill\n' \
-                        f'python dfa_sim_fit_one_withcondition_replicate.py --sample_size {sample_size} --theta {theta} --condition {condition} --replicate {replicate}'
+                        f'python sim_fit_one_replicate.py --sample_size {sample_size} --theta {theta} --condition {condition} --replicate {replicate}'
                     print(f"Launching {name}")
                     
                     sh_contents = f'#!/bin/bash\n' \
